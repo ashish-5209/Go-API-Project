88e21ca88742a34987133a74e89cea1cab0a26ef
Ashish saw <sheelaashish@Ashishs-MacBook-Air.local>
2024-05-24 16:09:02 +0530

    resolving error

--- heapDS/interface.go
+++ heapDS/interface.go
@@ -5,8 +5,8 @@ import (
 )
 
 type MaxHeapI interface {
-	BuildMaxHeap(ctx *context.Context, val int) (MaxHeap, error)
-	ExtractMax(ctx *context.Context) (int, error)
+	BuildMaxHeap(ctx *context.Context, val int)
+	ExtractMax(ctx *context.Context) int
 	MaxHeapifyUp(ctx *context.Context, idx int)
 	MaxHeapifyDown(ctx *context.Context, idx int)
 	Swap(ctx *context.Context, i1, i2 int)
